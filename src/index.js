5e98a338d785e4475506e9c4c2bc466c6485bfa0
Robby Thompson <robby@scriptdrop.co>
2018-08-07 15:23:21 -0400

    modify state from input

--- src/index.js
+++ src/index.js
@@ -9,11 +9,22 @@ class HelloUser extends React.Component {
     this.state = {
       username: 'the dude'
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({
+      username: e.target.value
+    })
   }
 
   render() {
     return (
-      <div>Hello {this.state.username}</div>
+      <div>
+        Hello {this.state.username} <br />
+        Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+      </div>
     )
   }
 }
