73af4174745c80fc602a5807237174f4fab89e2f
Robby Thompson <robby@scriptdrop.co>
2018-08-07 15:27:33 -0400

    basic props

--- src/index.js
+++ src/index.js
@@ -2,31 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class HelloUser extends React.Component {
-
-  constructor(props){
-    super(props)
-
-    this.state = {
-      username: 'the dude'
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(e){
-    this.setState({
-      username: e.target.value
-    })
-  }
-
   render() {
     return (
       <div>
-        Hello {this.state.username} <br />
-        Change Name: <input type="text" value={this.state.username} onChange={this.handleChange} />
+        Hello {this.props.name}
       </div>
     )
   }
 }
 
-ReactDOM.render(<HelloUser />, document.getElementById('root'));
+ReactDOM.render(<HelloUser name="Donnie"/>, document.getElementById('root'));
